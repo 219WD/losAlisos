7e32993bbcc6f8a9f4f9afdeaa6d20ae1853847a
219WD <macgunmanmc@gmail.com>
2024-11-05 19:20:35 -0300

    commiteada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeScreen from './pages/homeScreen';
+import HomeScreen from './pages/HomeScreen';
 import { Helmet } from 'react-helmet';
 
 function App() {
