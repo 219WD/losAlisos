0094a0f3a8e5ac8c0032857214ea18ce8a194ccf
219WD <macgunmanmc@gmail.com>
2024-11-05 18:55:20 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import HomeScreen from './pages/homeScreen.jsx'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './pages/homeScreen';
 import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Helmet>
         <title>Alisos Club - Asociación Civil | Cannabis Medicinal</title>
-        <meta name="description" content="Asociación Civil comprometida con el bienestar y la salud en la provincia de Tucumán, Argentina." />
+        <meta
+          name="description"
+          content="Asociación Civil comprometida con el bienestar y la salud en la provincia de Tucumán, Argentina."
+        />
         <script type="application/ld+json">
-          {`
-            {
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Alisos Club",
-              "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "25 de Mayo 123",
-                "addressLocality": "Tucumán",
-                "addressRegion": "T",
-                "postalCode": "4000",
-                "addressCountry": "AR"
-              },
-              "telephone": "+5493816492599",
-              "description": "Asociación Civil comprometida con el bienestar y la salud en la provincia de Tucumán, Argentina. Ofrecemos información, asistencia y productos de cannabis medicinal de alta calidad.",
-              "url": "https://alisosclub.vercel.app/"
-            }
-          `}
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Alisos Club",
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "25 de Mayo 123",
+              "addressLocality": "Tucumán",
+              "addressRegion": "T",
+              "postalCode": "4000",
+              "addressCountry": "AR",
+            },
+            "telephone": "+5493816492599",
+            "description":
+              "Asociación Civil comprometida con el bienestar y la salud en la provincia de Tucumán, Argentina. Ofrecemos información, asistencia y productos de cannabis medicinal de alta calidad.",
+            "url": "https://alisosclub.vercel.app/",
+          })}
         </script>
       </Helmet>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
       </Routes>
-    </Router>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
